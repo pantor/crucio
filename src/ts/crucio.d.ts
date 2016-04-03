9ce8cd0c78adf73d632eb78749eaad87f02abfe9
Lars <lbersch@gwdg.de>
2016-04-03 22:05:50 +0200

    fancier typescript

---
+++ src/ts/crucio.d.ts
@@ -0,0 +1,20 @@
+interface User {
+  active: number;
+  course_id: number;
+  email: string;
+  forgetValue: number;
+  group_id: number;
+  group_name?: string;
+  highlightExams: number;
+  last_sign_in: number;
+  password: string;
+  remember_me: string;
+  remember_user?: boolean;
+  repetitionValue: number;
+  semester: number;
+  showComments: number;
+  useAnswers: number;
+  useTags: number;
+  user_id: number;
+  username: string;
+}
